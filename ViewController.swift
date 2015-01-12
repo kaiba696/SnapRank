d7a29de7fc8f360125f96ab1fd7688cb36ade1b9
Kai Aichholz <kai.aichholz@gmail.com>
2015-01-12 12:38:36 -0800

    Commit -q -q
    Signed-off-by: Kai Aichholz <kai.aichholz@gmail.com>

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapRank
+//
+//  Created by Kai Aichholz on 1/12/15.
+//  Copyright (c) 2015 JOKA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
